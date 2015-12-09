37a125e14a987ff9ababbee0104d9c52da1b2115
heiheiha <1137029429@qq.com>
2015-12-09 18:42:20 +0800

    三重循环
    这个程序要实现的问题是有1，2，3，4四个数字，组成互不相同且不重复的三位数能有多少种，分别是什么？
    可以填在百位，十位，个位的数是1，2，3，4，通过三个变量来控制，用三重循环先组成所有的排列，然后再进行筛选，去掉那些不符合需求的。程序结果如下
    ：
    1,2,3
    1,2,4
    1,3,2
    1,3,4
    1,4,2
    1,4,3
    2,1,3
    2,1,4
    2,3,1
    2,3,4
    2,4,1
    2,4,3
    3,1,2
    3,1,4
    3,2,1
    3,2,4
    3,4,1
    3,4,2
    4,1,2
    4,1,3
    4,2,1
    4,2,3
    4,3,1
    4,3,2

--- three circulation/main.cpp
+++ three circulation/main.cpp
@@ -10,6 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int i,j,k;
+    printf("\n");
+    for(i=1;i<5;i++)
+        for(j=1;j<5;j++)
+            for(k=1;k<5;k++)
+            {
+                if(i!=j&&i!=k&&j!=k)
+                    printf("%d,%d,%d\n",i,j,k);
+            }
     return 0;
 }
