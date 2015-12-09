55134f57a5d1208b155eec16ffaf4a2bb9fbf6eb
heiheiha <1137029429@qq.com>
2015-12-09 18:27:55 +0800

    Initial Commit

---
+++ three circulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  three circulation
+//
+//  Created by  黄磊 on 15/12/9.
+//  Copyright © 2015年  黄磊. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
